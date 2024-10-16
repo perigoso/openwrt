3a7b0ee73f5206f1daa86dd3dfffc2055a2111ff
Rafael Silva <perigoso@riseup.net>
2024-10-16 15:12:59 +0100

    sunxi: add support for xr819 sdio wifi chip on OrangePi Zero
    Signed-off-by: Rafael Silva <perigoso@riseup.net>

--- target/linux/sunxi/image/cortexa7.mk
+++ target/linux/sunxi/image/cortexa7.mk
@@ -248,7 +248,7 @@ TARGET_DEVICES += xunlong_orangepi-r1
 define Device/xunlong_orangepi-zero
   DEVICE_VENDOR := Xunlong
   DEVICE_MODEL := Orange Pi Zero
-  DEVICE_PACKAGES:=kmod-rtc-sunxi
+  DEVICE_PACKAGES:=kmod-rtc-sunxi xr819-firmware kmod-xradio
   SOC := sun8i-h2-plus
 endef
 TARGET_DEVICES += xunlong_orangepi-zero
